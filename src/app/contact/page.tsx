ffbeed021685c09bb0be3591c3115c9c3c5a0b97
Young KIM <youngkim0224@gmail.com>
2026-04-25 22:57:06 +0900

    fix: 문의하기 /contact 페이지 추가, 버튼 크기 통일

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,9 @@
+import { ContactSection } from '@/components/home/ContactSection'
+
+export default function ContactPage() {
+  return (
+    <div style={{ paddingTop: 80 }}>
+      <ContactSection />
+    </div>
+  )
+}
