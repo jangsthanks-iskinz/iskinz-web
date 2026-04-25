ef67092a3723b7007a1e937c45af3e6d31a33341
Young KIM <youngkim0224@gmail.com>
2026-04-25 22:07:49 +0900

    fix: TextStyle import 수정

--- src/components/admin/RichEditor.tsx
+++ src/components/admin/RichEditor.tsx
@@ -3,7 +3,7 @@ import { useEditor, EditorContent } from '@tiptap/react'
 import StarterKit from '@tiptap/starter-kit'
 import Image from '@tiptap/extension-image'
 import TextAlign from '@tiptap/extension-text-align'
-import TextStyle from '@tiptap/extension-text-style'
+import { TextStyle } from '@tiptap/extension-text-style'
 import Color from '@tiptap/extension-color'
 import { useCallback, useRef } from 'react'
 
